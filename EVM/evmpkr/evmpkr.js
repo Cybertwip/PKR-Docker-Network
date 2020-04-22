21f484abdece151fd617e460185de1cc0a64987a
Victor Lopez <vjlopezcarrillo@gmail.com>
2020-04-22 07:30:12 -0600

    Remove null check operator (it is not supported in JavaScript

--- EVM/evmpkr/evmpkr.js
+++ EVM/evmpkr/evmpkr.js
@@ -38,6 +38,139 @@ var EVMPKR = class {
     }
   }
 
+  async setSCAddress(stub, args){
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    const sc = {
+      address: args[0]
+    };
+
+    await stub.putState('SCADDRESS', Buffer.from(JSON.stringify(sc)));
+
+  }
+
+  async getSCAddress(stub) {
+
+    const scAsBytes = await stub.getState('SCADDRESS'); 
+    if (!scAsBytes || scAsBytes.length === 0) {
+        throw new Error(`SCADDRESS does not exist`);
+    }
+    console.log(scAsBytes.toString());
+
+      const strValue = Buffer.from(scAsBytes).toString('utf8');
+      let sc;
+      try {
+          sc = JSON.parse(strValue);
+      } catch (err) {
+          console.log(err);
+          sc = strValue;
+      }
+
+    if(sc.address){
+      return sc.address;
+    } else {
+      return sc;
+    }
+
+  }
+
+  async setEVMAddress(stub, args){
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    const evm = {
+      address: args[0]
+    };
+
+    await stub.putState('EVMADDRESS', Buffer.from(JSON.stringify(evm)));
+
+  }
+
+
+  async getEVMAddress(stub) {
+
+    const evmAsBytes = await stub.getState('EVMADDRESS'); 
+    if (!evmAsBytes || evmAsBytes.length === 0) {
+        throw new Error(`EVMADDRESS does not exist`);
+    }
+    console.log(evmAsBytes.toString());
+
+    const strValue = Buffer.from(evmAsBytes).toString('utf8');
+    let evm;
+    try {
+        evm = JSON.parse(strValue);
+    } catch (err) {
+        console.log(err);
+        evm = strValue;
+    }
+
+    if(evm.address){
+      return evm.address;
+    } else {
+      return evm;
+    }
+
+  }
+
+  async connectToFab3(stub){
+    const evmAddress = await getEVMAddress(stub);
+    web3 = new Web3(evmAddress)
+  }
+
+  async validateRequirements(stub){
+    const evmAsBytes = await stub.getState('EVMADDRESS'); 
+    if (!evmAsBytes || evmAsBytes.length === 0) {
+        throw new Error(`EVMADDRESS does not exist`);
+    }
+
+    const scAsBytes = await stub.getState('SCADDRESS'); 
+    if (!scAsBytes || scAsBytes.length === 0) {
+        throw new Error(`SCADDRESS does not exist`);
+    }
+
+    if(!web3){
+        throw new Error(`Fab3 not connected`);      
+    }
+
+    return true;
+  }
+
+  async setGreeting(stub, args){
+    console.info('============= START : Set Greeting ===========');
+
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    await validateRequirements(stub);
+
+    const contractAddress = await getSCAddress(stub);
+    const greeterContract = new web3.eth.Contract(require(path.join(__dirname, "abi.json")), contractAddress)
+
+    await greeterContract.methods.set(args[0]).send({from: account.address});
+
+    console.info('============= END : Set Greeting ===========');
+  }
+
+  async getGreeting(stub){
+    console.info('============= START : Get Greeting ===========');
+
+
+    await validateRequirements(stub);
+      
+    const contractAddress = await getSCAddress(stub);
+    const greeterContract = new web3.eth.Contract(require(path.join(__dirname, "abi.json")), contractAddress)
+
+    const greet = await greeterContract.methods.greet().call({from: account.address}) //Call to greet function
+
+
+    console.info('============= END : Get Greeting ===========');
+                
+    return greet;
+  }
 
 };
 
