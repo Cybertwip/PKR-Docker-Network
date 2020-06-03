f49c71808b51cb4e1b4e1fb4f99f2d01d7ab8d1b
Victor Lopez <vjlopezcarrillo@gmail.com>
2020-06-02 19:08:22 -0600

    New chaincode directory structure

---
+++ EVM/chaincode/hold-em/hold-em.js
@@ -0,0 +1,219 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+const shim = require('fabric-shim');
+const util = require('util');
+
+const Web3 = require('web3')
+const path = require('path')
+
+var EVMPKR = class {
+
+  // Initialize the chaincode
+  async Init(stub) {
+    console.info('========= Hold Em Init =========');
+    return shim.success();
+  }
+
+  async Invoke(stub) {
+    let ret = stub.getFunctionAndParameters();
+    console.info(ret);
+    let method = this[ret.fcn];
+    if (!method) {
+      console.log('no method of name:' + ret.fcn + ' found');
+      return shim.success();
+    }
+    try {
+      let payload = await method(stub, ret.params);
+      return shim.success(payload);
+    } catch (err) {
+      console.log(err);
+      return shim.error(err);
+    }
+  }
+  
+  async Create(stub, args){
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    var result = { status: 'Correct', description: 'Game created successfully'}
+
+    try {
+      let gameData = JSON.parse(args[0]);
+
+      gameData.playing = true;
+
+      await stub.putState("Game:" + gameData.id, Buffer.from(JSON.stringify(gameData)));
+
+    } catch(err){
+      console.log(err);
+      result.status = 'Error';
+      result.description = 'Failed to parse JSON input';
+    }
+
+    return Buffer.from(JSON.stringify(result));
+    
+  }
+
+  async Play(stub, args){
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    var result = { status: 'Correct', description: 'Bet correctly' }
+
+    let bet = {}
+
+    try{
+      bet = JSON.parse(args[0]);
+    } catch(err){
+      console.log(err);
+      result.status = 'Error';
+      result.description = 'Failed to parse JSON input';
+      return Buffer.from(JSON.stringify(result));
+    }
+
+    try{
+
+      const gameAsBytes = await stub.getState('GAME:' + bet.gameId); 
+      if (!gameAsBytes || gameAsBytes.length === 0) {
+          throw new Error(`Game does not exist`);
+      }      
+
+      const gameData = JSON.parse(gameAsBytes);
+
+
+      if(!gameData.playing){
+        result.status = 'Error';
+        result.description = 'Game is not playing anymore';
+        throw new Error('Game already played');
+      }
+
+      var filteredBets = gameData.bets.filter(bet => bet.action != 2); // different than abandon
+
+      switch(bet.action){
+          case 0:{ // small blind
+              if(filteredBets.length != 0){
+                  result.status = "Error";
+                  result.description = "Can't make small blind while other bets already took place";
+                  throw new Error('Not acceptable exception');
+              }
+          }
+          break;
+
+          case 1:{ // big blind
+              if(filteredBets.length == 0){
+                  result.status = "Error";
+                  result.description = "Big blind bet requires a small blind bet before";
+                  throw new Error('Not acceptable exception');
+              } 
+
+              var previousBet = filteredBets[filteredBets.length - 1];
+              if(previousBet.action != BetAction.SmallBlind){
+                  result.status = "Error";
+                  result.description = "Big blind bet requires a small blind bet before";
+                  throw new Error('Not acceptable exception');
+              }
+
+          
+          }
+          break;
+
+          case 2:{ // abandon
+              for(var i = 0; i<gameData.players.length; ++i){
+                  if(gameData.players[i].id == bet.playerId){
+                      gameData.players[i].status = PlayerStatus.Left;
+                  }
+              }
+          }
+          break;
+
+
+          case 3:{ // skip
+              if(filteredBets.length == 0){
+                  result.status = "Error";
+                  result.description = "Can't skip if no previous bets are made";
+
+                  throw new Error('Not acceptable exception');
+              } 
+
+              var previousBet = filteredBets[filteredBets.length - 1];
+              if(previousBet.action == BetAction.Normal || previousBet.action == BetAction.Rise){
+                  result.status = "Error";
+                  result.description = "Can't skip if previous bet is normal bet or rise bet";
+
+                  throw new Error('Not acceptable exception');
+              }
+          }
+          break;          
+
+          case 4:{ // normal
+              if(filteredBets.length == 0){
+                  result.status = "Error";
+                  result.description = "Can't bet if no previous big blind bet is made";
+                  throw new Error('Not acceptable exception');
+
+
+              } 
+
+              var previousBet = filteredBets[filteredBets.length - 1];
+              if(previousBet.action == BetAction.Rise){
+
+                  result.status = "Error";
+                  result.description = "Can't bet normally if previous bet is a rise bet";
+                  throw new Error('Not acceptable exception');
+
+              }
+          }
+          break;
+
+      }
+
+      gameData.bets.push(bet);
+      gameData.pot = gameData.pot + parseInt(bet.amount.toString());
+
+      await stub.putState('Game:' + bet.gameId, Buffer.from(JSON.stringify(gameData)));
+
+    } catch (err) {
+      console.log(err);
+    }
+
+    return Buffer.from(JSON.stringify(result));
+  }
+
+  async Finish(stub, args){
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    let veredict = JSON.parse(args[0])
+
+    let id = veredict.gameId;
+    let winnerId = veredict.winnerId;
+
+    const gameAsBytes = await stub.getState('GAME:' + id); 
+    if (!gameAsBytes || gameAsBytes.length === 0) {
+        throw new Error(`Game does not exist`);
+    }
+
+    const gameObject = JSON.parse(gameAsBytes);
+
+    gameData.playing = false;
+    gameData.winnerId = winnerId;
+
+    await stub.putState('GAME:' + id, Buffer.from(JSON.stringify(gameData)));
+
+    return Buffer.from(JSON.stringify(gameData));
+
+  }
+
+
+
+
+};
+
+shim.start(new EVMPKR());
---
+++ EVM/chaincode/pkrstudio/pkrstudio.js
@@ -0,0 +1,436 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+const shim = require('fabric-shim');
+const util = require('util');
+
+const Web3 = require('web3')
+const path = require('path')
+
+var EVMPKR = class {
+
+  // Initialize the chaincode
+  async Init(stub) {
+    console.info('========= EVMPKR Init =========');
+    return shim.success();
+  }
+
+  async Invoke(stub) {
+    let ret = stub.getFunctionAndParameters();
+    console.info(ret);
+    let method = this[ret.fcn];
+    if (!method) {
+      console.log('no method of name:' + ret.fcn + ' found');
+      return shim.success();
+    }
+    try {
+      let payload = await method(stub, ret.params);
+      return shim.success(payload);
+    } catch (err) {
+      console.log(err);
+      return shim.error(err);
+    }
+  }
+
+
+  async User(stub, args){
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+
+    var result = { status: 'Correct', description: 'User retrieval is successful' }
+
+    //let gameId = args[0];
+    let userId = args[0];
+
+    let userData = {}
+
+    const userAsBytes = await stub.getState('USER:' + userId); 
+    if (!userAsBytes || userAsBytes.length === 0) {
+        result.status = 'Error';
+        result.description = 'User not found';
+    } else {
+      result.status = 'Correct';
+      result.description = 'User found';
+
+      userData = JSON.parse(userAsBytes);
+    }
+
+    if(result.status != 'Correct'){
+      return Buffer.from(JSON.stringify(result));      
+    }
+    else{
+      return Buffer.from(JSON.stringify(userData));
+    }
+
+  }
+
+
+  async RegisterUser(stub, args){
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+
+    var result = { status: 'Correct', description: 'Register user is successful' }
+
+
+    let userData = {}
+
+    try{
+      userData = JSON.parse(args[0]);
+    } catch(err){
+      console.log(err);
+      result.status = 'Error';
+      result.description = 'Failed to parse JSON input';
+      return Buffer.from(JSON.stringify(result));
+    }
+
+    userData.tokens = 1000;
+
+    await stub.putState('USER:' + userData.id, Buffer.from(JSON.stringify(userData)));
+
+    return Buffer.from(JSON.stringify(result));
+  }
+
+  async Create(stub, args){
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+
+    const channelName = 'pkr';
+
+    const gameIdentifier = args[0];
+
+    var result = { status: 'Error', description: "Game not found"}
+
+    if(gameIdentifier == 'hold-em'){
+      const chaincodeName = gameIdentifier;
+      const functionArgs = ['Create', args[1]];
+      chaincodeResult = await stub.invokeChaincode(chaincodeName, functionArgs, channelName);
+      result = JSON.parse(chaincodeResult.payload.toString('utf8'));
+    }
+
+    return Buffer.from(JSON.stringify(result));
+  }
+
+  async Play(stub, args){
+    if (args.length != 4) {
+      throw new Error('Incorrect number of arguments. Expecting 4');
+    }
+
+    var result = { status: 'Error', description: "Game not found"}
+
+
+    const channelName = 'pkr';
+
+    const gameIdentifier = args[0];
+
+    const tokens = parseInt(args[1].toString());
+
+    const userId = args[2];
+
+    const userAsBytes = await stub.getState('USER:' + userId); 
+    if (!userAsBytes || userAsBytes.length === 0) {
+        result.status = 'Error';
+        result.description = 'User not found';
+    } else {
+      result.status = 'Correct';
+      result.description = 'User found';
+
+      const userData = JSON.parse(userAsBytes);
+      userData.tokens = userData.tokens - tokens;
+
+      if(userData.tokens < 0){
+        userData.tokens = 0;
+        result.status = 'Error';
+        result.description = 'User does not have enough funds to perform the game';
+      }
+
+      await stub.putState('USER:' + userData.id, Buffer.from(JSON.stringify(userData)));
+
+    }
+
+    if(result.status == 'Error'){
+      return Buffer.from(JSON.stringify(result));
+    }
+
+    if(gameIdentifier == 'hold-em'){
+      const chaincodeName = gameIdentifier;
+      const functionArgs = ['Play', args[3]];
+      chaincodeResult = await stub.invokeChaincode(chaincodeName, functionArgs, channelName);
+      result = JSON.parse(chaincodeResult.payload.toString('utf8'));
+    }
+
+    return Buffer.from(JSON.stringify(result));
+  }
+
+  async Finish(stub, args){
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+
+    const channelName = 'pkr';
+
+    const gameIdentifier = args[0];
+
+    var result = { status: 'Error', description: "Game not found"}
+
+    if(gameIdentifier == 'hold-em'){
+      const chaincodeName = gameIdentifier;
+      const functionArgs = ['Finish', args[1]];
+      chaincodeResult = await stub.invokeChaincode(chaincodeName, functionArgs, channelName);
+      result = JSON.parse(chaincodeResult.payload.toString('utf8'));
+
+      if(!result.winnerId){
+        result.status = 'Error';
+        result.description = 'Error while processing winner';
+      }
+      else{
+        let userId = result.winnerId;
+        let pot = result.pot;
+
+        const userAsBytes = await stub.getState('USER:' + userId); 
+        if (!userAsBytes || userAsBytes.length === 0) {
+            result.status = 'Error';
+            result.description = 'User not found';
+        } else {
+          result.status = 'Correct';
+          result.description = 'Winner found awarded: ' + pot.toString() + ' tokens';
+
+          const userData = JSON.parse(userAsBytes);
+          userData.tokens = userData.tokens + pot;
+
+          await stub.putState('USER:' + userData.id, Buffer.from(JSON.stringify(userData)));
+
+        }
+
+
+      }
+
+    }
+
+    return Buffer.from(JSON.stringify(result));
+  }
+
+  async SetEVMAddress(stub, args){
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting 1');
+    }
+
+    await stub.putState('EVMADDRESS', Buffer.from(JSON.stringify({address: args[0]})));
+
+  }
+
+
+  async GetEVMAddress(stub) {
+    console.info('============= START : getEVMAddress ===========');
+
+
+    const evmAsBytes = await stub.getState('EVMADDRESS'); 
+    if (!evmAsBytes || evmAsBytes.length === 0) {
+        throw new Error(`EVMADDRESS does not exist`);
+    }
+
+    var evmAddress = JSON.parse(evmAsBytes);
+
+    console.info(evmAddress);
+
+    console.info('============= END : getEVMAddress ===========');
+
+    return Buffer.from(JSON.stringify(evmAddress.address));
+
+
+  }
+
+  async DeploySC(stub, args){
+    console.info('============= START : deploySC ===========');
+
+    if (args.length != 3) {
+      throw new Error('Incorrect number of arguments. Expecting 3');
+    }
+
+    const evmAsBytes = await stub.getState('EVMADDRESS'); 
+    if (!evmAsBytes || evmAsBytes.length === 0) {
+        throw new Error(`EVMADDRESS does not exist`);
+    }
+
+    var evmAddress = JSON.parse(evmAsBytes);
+
+
+    console.info('============= Setup Web3 ===========');
+
+    console.info('============= Web3 address ===========');
+
+    console.info('Address: ' + evmAddress);
+    console.info('Address: ' + evmAddress.toString());
+
+    //evmAddress = "http://docker.for.mac.localhost:5000";
+
+    var web3 = new Web3(evmAddress);
+
+    console.info('============= Get accounts ===========');
+
+
+    let accounts = await web3.eth.getAccounts();
+
+    const contractName = args[0];
+    const contractABI  = JSON.parse(args[1]);
+    const contractCode = args[2];
+
+    console.info('============= Instantiate contract ===========');
+
+    const newContract = new web3.eth.Contract(contractABI);
+
+    console.info('============= Deploy contract ===========');
+
+    var contractInstancePromise = newContract.deploy({ data: contractCode });
+    
+
+    console.info('============= Send promise ===========');
+
+    var contractInstance = await contractInstancePromise.send({
+      from: accounts[0]
+    });
+ 
+    //var contractInstance = await contractInstancePromise.send();
+
+
+    console.info('============= Store address ===========');
+
+    await stub.putState(contractName, Buffer.from(contractInstance.options.address));
+
+    console.info('============= Store ABI ===========');
+
+    await stub.putState(contractName + '_ABI', Buffer.from(JSON.stringify(contractABI)));
+
+    console.info('============= END : deploySC ===========');
+
+  }
+
+
+  async Query(stub, args){
+    console.info('============= START : query ===========');
+
+    const evmAsBytes = await stub.getState('EVMADDRESS'); 
+    if (!evmAsBytes || evmAsBytes.length === 0) {
+        throw new Error(`EVMADDRESS does not exist`);
+    }
+
+    const evmAddress = JSON.parse(evmAsBytes);
+
+    var web3 = new Web3(evmAddress);
+    
+    let accounts = await web3.eth.getAccounts();
+
+    const contractName = args[0];
+
+    const queryMethod = args[1];
+
+    const contractAddressAsBytes = await stub.getState(contractName); 
+    if (!contractAddressAsBytes || contractAddressAsBytes.length === 0) {
+        throw new Error(`Contract address does not exist, i.e. might not be deployed yet`);
+    }
+
+    const contractABIAsBytes = await stub.getState(contractName + '_ABI'); 
+    if (!contractABIAsBytes || contractABIAsBytes.length === 0) {
+        throw new Error(`Contract ABI does not exist, i.e. might not be deployed yet`);
+    }
+
+    const contractAddress = contractAddressAsBytes.toString();
+
+    const contractABI = JSON.parse(contractABIAsBytes.toString());
+
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+    const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
+    //const encodeSelector = f => web3.sha3(f).slice(0,10)
+
+    const output = contractABI
+        .filter(e => e.type === "function" && e.name === queryMethod)
+        .flatMap(e => `${prepareData(e)}`)
+
+    if(output.length == 1){
+
+      var methodPromise = contract.methods[output[0]].apply(contract, args.slice(2, args.length));
+      var queryResult = await methodPromise.call({from: accounts[0]});
+
+      console.info('============= END : query ===========');
+
+      return queryResult;
+
+    } else {
+
+      console.info('============= END : query ===========');
+
+      throw new Error(`Called method ${queryMethod} does not exist or more than one methods found with the same name`);
+    }
+  }
+
+
+
+  async Transact(stub, args){
+    console.info('============= START : transact ===========');
+    
+    const evmAsBytes = await stub.getState('EVMADDRESS'); 
+    if (!evmAsBytes || evmAsBytes.length === 0) {
+        throw new Error(`EVMADDRESS does not exist`);
+    }
+
+    const evmAddress = JSON.parse(evmAsBytes);
+
+    var web3 = new Web3(evmAddress);
+    
+    let accounts = await web3.eth.getAccounts();
+
+    const contractName = args[0];
+
+    const transactMethod = args[1];
+
+    const contractAddressAsBytes = await stub.getState(contractName); 
+    if (!contractAddressAsBytes || contractAddressAsBytes.length === 0) {
+        throw new Error(`Contract address does not exist, i.e. might not be deployed yet`);
+    }
+
+    const contractABIAsBytes = await stub.getState(contractName + '_ABI'); 
+    if (!contractABIAsBytes || contractABIAsBytes.length === 0) {
+        throw new Error(`Contract ABI does not exist, i.e. might not be deployed yet`);
+    }
+
+    const contractAddress = contractAddressAsBytes.toString();
+
+    const contractABI = JSON.parse(contractABIAsBytes.toString());
+
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+    const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
+    //const encodeSelector = f => web3.sha3(f).slice(0,10)
+
+    const output = contractABI
+        .filter(e => e.type === "function" && e.name === transactMethod)
+        .flatMap(e => `${prepareData(e)}`)
+
+    if(output.length == 1){
+
+      var methodPromise = contract.methods[output[0]].apply(contract, args.slice(2, args.length));
+      var queryResult = await methodPromise.send({from: accounts[0]});
+
+      console.info('============= END : transact ===========');
+
+    } else {
+
+      console.info('============= END : transact ===========');
+
+      throw new Error(`Called method ${transactMethod} does not exist or more than one methods found with the same name`);
+    }
+  }
+
+
+
+
+
+};
+
+shim.start(new EVMPKR());
